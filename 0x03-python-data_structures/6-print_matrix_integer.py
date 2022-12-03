f6bea23080d858d7ba08676f451a122becd3e433
umjoji <30466162+umjoji@users.noreply.github.com>
2022-12-03 11:21:20 +0100

    Changes to be committed: 	modified:   0x01-python-if_else_loops_functions/3-print_alphabt.py 	modified:   0x03-python-data_structures/10-divisible_by_2.py 	modified:   0x03-python-data_structures/11-delete_at.py 	modified:   0x03-python-data_structures/6-print_matrix_integer.py 	modified:   0x03-python-data_structures/8-multiple_returns.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,13 +7,3 @@ def print_matrix_integer(matrix=[[]]):
             else:
                 print("{}".format(element), end="")
         print()
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,11 +3,3 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         return len(sentence), None
     return len(sentence), sentence[0]
-
-sentence = "At school, I learnt C!"
-sentence1 = ""
-length, first = multiple_returns(sentence)
-length1, first1 = multiple_returns(sentence1)
-
-print("Length: {:d} - First character: {}".format(length, first))
-print("Length: {:d} - First character: {}".format(length1, first1))
