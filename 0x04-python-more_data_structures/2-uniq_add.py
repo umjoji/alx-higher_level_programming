e72a2c14032504bad88ace6df6b2ca598bc1bb70
umjoji <30466162+umjoji@users.noreply.github.com>
2022-12-14 16:41:38 +0100

    Changes to be committed: 	new file:   0x04-python-more_data_structures/0-square_matrix_simple.py 	new file:   0x04-python-more_data_structures/1-search_replace.py 	new file:   0x04-python-more_data_structures/2-uniq_add.py 	new file:   0x04-python-more_data_structures/3-common_elements.py 	new file:   0x04-python-more_data_structures/4-only_diff_elements.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    # Create a set to store the unique integers
+    unique_integers = set()
+    
+    # Iterate over the list
+    for i in my_list:
+        # Check if the current element is an integer and add it to the set
+        # if it is not already in the set
+        if isinstance(i, int) and i not in unique_integers:
+            unique_integers.add(i)
+    
+    # Return the sum of the elements in the set
+    return sum(unique_integers)
