459806260f3cdf8f0c16afa62bdaebc242a255f5
umjoji <30466162+umjoji@users.noreply.github.com>
2023-03-14 16:30:48 +0100

    Add project files and README

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// prints 3 lines
+// C is fun, python is cool and javascript is amazing
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
