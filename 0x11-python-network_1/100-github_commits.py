3d6b27104dd2f2f6df4a1e9128d9ecc1d9b07d0d
umjoji <30466162+umjoji@users.noreply.github.com>
2023-04-02 16:33:19 +0100

    Add project files and Readme

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""lists the 10 most recent commits
+on a given GitHub repository.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    response = requests.get(url)
+    commits = response.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
